5f92d2a687a30ddcbdcb45df33d266200e52dd91
kobi <kobishaked1@gmail.com>
2020-05-02 01:08:03 +0300

    Updates

---
+++ precache-manifest.62e560f97f7619164010046de2e76b85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412d365445575dca5f58f2d1990931b3",
+    "url": "/painting-group/index.html"
+  },
+  {
+    "revision": "917a9950d5e1cd7c58a2",
+    "url": "/painting-group/static/css/main.4cd7b08f.chunk.css"
+  },
+  {
+    "revision": "d6906219b76949d3875d",
+    "url": "/painting-group/static/js/2.db1b8e21.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/painting-group/static/js/2.db1b8e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917a9950d5e1cd7c58a2",
+    "url": "/painting-group/static/js/main.cccff0e5.chunk.js"
+  },
+  {
+    "revision": "cbe92902378008933bee",
+    "url": "/painting-group/static/js/runtime-main.fc60f080.js"
+  }
+]);
